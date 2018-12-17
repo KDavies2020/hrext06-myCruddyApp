18c0f0e7fa2ba20108e53a5d919feb84ba33d0ab
Kyle Davies <kyledavies@Kyles-MacBook-Air.local>
2018-12-17 11:47:42 -0800

    Working on our btn-submit to add multiple values. Currently concatting onto our value - however display is a little rough and I am having to hardcode in slicing out 'undefined'

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ $(document).ready(function(){
 
   // write to local storage from input when button save clicked
   $('.btn-submit').on('click', function(){
-    localStorage.setItem('inputFieldValue', $('.text-entry').val());
+    // localStorage.setItem('inputFieldValue', $('.text-entry').val());
+    // var myItemInStorage = localStorage.getItem('inputFieldValue');
+    if(localStorage.length === undefined){
+  localStorage.setItem('inputFieldValue', $('.text-entry').val());
+} else{ localStorage['inputFieldValue'] = [localStorage['inputFieldValue']].concat($('.text-entry').val());
+
     var myItemInStorage = localStorage.getItem('inputFieldValue');
+}
+    //console.log(localStorage.length)
     console.log('myItemInStorage', myItemInStorage);
 
     // display the value here
-    $('.list-display-field').text(myItemInStorage); // ??
+    $('.list-display-field').text(myItemInStorage.slice(9)); // ??
 
   });
 
