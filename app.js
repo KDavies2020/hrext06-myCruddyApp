29011729036d5846b1a16e1f7e0d1548afc016e0
Kyle Davies <kyledavies@kyles-air.attlocal.net>
2018-12-20 10:28:41 -0600

    decided to turn scheduled games into divs for easier style and starting new line. basic funcionality / MVP working. Need to add styling and additional UI additions.

--- app.js
+++ app.js
@@ -2,16 +2,15 @@ $(document).ready(function(){
   console.log('jQuery loaded');
   localStorage.clear();
   var storageArray = [];
+  $('.text-entry').val(placeholderValueQuestions[0]);
 
 
 
   // write to local storage from input when button save clicked
   //save in array to access all inputs
   $('.btn-submit').on('click', function(){
-
   storageArray.push($('.text-entry').val())
   localStorage.setItem('inputFieldValue', storageArray)
-  //$('.list-display-field').text(storageArray);
   $('.text-entry').val(createInputPrompts(placeholderValueQuestions))
   });
 
@@ -27,12 +26,21 @@ $(document).ready(function(){
     $('.list-display-field').html('');
     localStorage.clear();
   });
-
-  $('.btn-schedule').on('click', function(){
-    $('.list-display-field').text(scheduler(storageArray))
-    $('.text-entry').val('');
-
-
+  //when we are done with inputs, our schedule button will clear the holder text in the display field, and display newly scheduled games.
+   $('.btn-schedule').one('click', function(){
+    $('.list-display-field').text('')
   })
+  $('.btn-schedule').on('click', function(){
+    $('.list-display-field').append(`<div class = "scheduled-game">${scheduler(storageArray)} </div>`)
+    $('.text-entry').val(placeholderValueQuestions[0]);
+    i = 0;
+  })
+
+  //clears text-entry upon click so that you dont have to manually remove prompts.
+  $('.text-entry').on('click', function(){
+    $('.text-entry').val('')
+  })
+
+
 
 });
--- helpfulFunctions.js
+++ helpfulFunctions.js
@@ -1,6 +1,6 @@
 var i = 0;
 
-var placeholderValueQuestions = ["Your team name","Home or Away?", "Date of game (DD/MM)", "Time of game (ex. 9:30 AM)", "Location", "Opponent name"];
+var placeholderValueQuestions = ["Your team name","Home or Away?", "Date of game (MM/DD)", "Time of game (ex. 9:30 AM)", "Location", "Opponent name"];
 
 var scheduler = function(array){
   var teamName = array[0];
@@ -11,9 +11,9 @@ var scheduler = function(array){
   var opponent = array[5];
 
   if(homeOrAway === 'Home' || homeOrAway === 'home'){
-    return `Home game on ${date} scheduled for ${time} at ${location}. ${teamName} vs. ${opponent}.`
+    return `Home game on ${date} scheduled for ${time} at ${location}. ${teamName} vs. ${opponent}.` + '/n'
   } else {
-    return `Away game on ${date} scheduled for ${time} at ${location}. ${teamName} @ ${opponent}.`
+    return `Away game on ${date} scheduled for ${time} at ${location}. ${teamName} @ ${opponent}.` + '/n'
   }
 }
 
@@ -21,11 +21,14 @@ var scheduler = function(array){
 // upon clicked submit game - we need to pass our storageArray into scheduler function.
 
 // second function that will change our placeholder / input value to the element of our placeholderValueQuestions array.
-var i = 0;
+
 var createInputPrompts = function(array){
-  if (i < array.length-1){i++;
+  if (i < array.length-1){
+    i++;
   return array[i];
-  } else{
+  } else if( i === array.length-1){
     return "Click 'Schedule Game'"
   }
 }
+
+
